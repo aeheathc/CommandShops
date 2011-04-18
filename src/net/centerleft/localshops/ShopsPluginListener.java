763fac80164ddf770f0f6664ce3f4bb10892c043
Morgan Humes <mrh9724@lanaddict.com>
2011-04-18 02:23:02 -0400

    Use Logger instead of stdout

--- src/net/centerleft/localshops/ShopsPluginListener.java
+++ src/net/centerleft/localshops/ShopsPluginListener.java
@@ -1,5 +1,7 @@
 package net.centerleft.localshops;
 
+import java.util.logging.Logger;
+
 import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.event.server.ServerListener;
@@ -9,48 +11,52 @@ import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class ShopsPluginListener extends ServerListener {
-	private final LocalShops plugin;
-	
-	protected iConomy iConomy;
-	protected Permissions permissions;
-	protected PermissionHandler gmPermissionCheck; 
-	protected boolean usePermissions = false;
-	protected boolean useiConomy = false;
-	
-	public ShopsPluginListener(LocalShops instance) { 
-		plugin = instance;
+    private final LocalShops plugin;
+
+    // Logging
+    private final Logger log = Logger.getLogger("Minecraft");
+
+    // Attributes
+
+    protected iConomy iConomy;
+    protected Permissions permissions;
+    protected PermissionHandler gmPermissionCheck;
+    protected boolean usePermissions = false;
+    protected boolean useiConomy = false;
+
+    public ShopsPluginListener(LocalShops instance) {
+	plugin = instance;
+    }
+
+    @Override
+    public void onPluginEnable(PluginEnableEvent event) {
+	if (event.getPlugin().getDescription().getName().equals("iConomy")) {
+	    iConomy = (iConomy) event.getPlugin();
+	    System.out.println("LocalShops: Attached to iConomy.");
+	    useiConomy = true;
+	    ShopData.currencyName = iConomy.getBank().getCurrency();
 	}
 
-	@Override
-	public void onPluginEnable(PluginEnableEvent event) {
-        if(event.getPlugin().getDescription().getName().equals("iConomy")) {
-            iConomy = (iConomy)event.getPlugin();
-            System.out.println("LocalShops: Attached to iConomy.");
-            useiConomy = true;
-    		ShopData.currencyName = iConomy.getBank().getCurrency();
-        }
-        
-        if(event.getPlugin().getDescription().getName().equals("Permissions")) {
-        	permissions = (Permissions)event.getPlugin();
-            gmPermissionCheck = permissions.getHandler();
-        	System.out.print("LocalShops: Attached to Permissions");
-        	usePermissions = true;
-            
-            
-        } 
+	if (event.getPlugin().getDescription().getName().equals("Permissions")) {
+	    permissions = (Permissions) event.getPlugin();
+	    gmPermissionCheck = permissions.getHandler();
+	    log.info(String.format("[%s] %s", plugin.pdfFile.getName(), "Attached to Permissions"));
+	    usePermissions = true;
+
+	}
     }
-    
-	@Override
+
+    @Override
     public void onPluginDisable(PluginDisableEvent event) {
-    	if(event.getPlugin().getDescription().getName().equals("iConomy")) {
-            iConomy = null;
-            System.out.println("LocalShops: Lost connection to iConomy.");
-            useiConomy = false;
-    	}
-        if(event.getPlugin().getDescription().getName().equals("Permissions")) {
-        	permissions = (Permissions)event.getPlugin();
-        	System.out.print("LocalShops: Lost connection to Permissions");
-        	usePermissions = false;
-        }
+	if (event.getPlugin().getDescription().getName().equals("iConomy")) {
+	    iConomy = null;
+	    System.out.println("LocalShops: Lost connection to iConomy.");
+	    useiConomy = false;
+	}
+	if (event.getPlugin().getDescription().getName().equals("Permissions")) {
+	    permissions = (Permissions) event.getPlugin();
+	    System.out.print("LocalShops: Lost connection to Permissions");
+	    usePermissions = false;
+	}
     }
 }
