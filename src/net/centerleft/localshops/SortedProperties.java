ea4994ffb64fd95b31e1139cb2f7325f4488711f
Morgan Humes <mrh9724@lanaddict.com>
2011-05-01 18:58:59 -0400

    Sort Properties files

---
+++ src/net/centerleft/localshops/SortedProperties.java
@@ -0,0 +1,19 @@
+package net.centerleft.localshops;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.Vector;
+
+public class SortedProperties extends Properties {
+    public Enumeration keys() {
+       Enumeration keysEnum = super.keys();
+       Vector<String> keyList = new Vector<String>();
+       while(keysEnum.hasMoreElements()){
+         keyList.add((String)keysEnum.nextElement());
+       }
+       Collections.sort(keyList);
+       return keyList.elements();
+    }
+    
+  }
