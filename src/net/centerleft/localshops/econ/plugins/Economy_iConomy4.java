de77b2b65c94ce9c91f9f030d1689624b5ec43b5
Morgan Humes <mrh9724@lanaddict.com>
2011-05-03 15:44:23 -0400

    iConomy 4/5 Support

---
+++ src/net/centerleft/localshops/econ/plugins/Economy_iConomy4.java
@@ -0,0 +1,131 @@
+package net.centerleft.localshops.econ.plugins;
+
+import net.centerleft.localshops.LocalShops;
+import net.centerleft.localshops.Shop;
+import net.centerleft.localshops.econ.Economy;
+
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event.Type;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.event.server.ServerListener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+
+import com.nijiko.coelho.iConomy.iConomy;
+import com.nijiko.coelho.iConomy.system.Account;
+
+public class Economy_iConomy4 implements Economy {
+    private String name = "iConomy 4";
+    private LocalShops plugin = null;
+    private PluginManager pluginManager = null;
+    protected iConomy economy = null;
+    private EconomyServerListener economyServerListener = null;
+    
+    public Economy_iConomy4(LocalShops plugin) {
+        this.plugin = plugin;
+        this.pluginManager = this.plugin.getServer().getPluginManager();
+
+        economyServerListener = new EconomyServerListener(this);
+        
+        this.pluginManager.registerEvent(Type.PLUGIN_ENABLE, economyServerListener, Priority.Monitor, plugin);
+        this.pluginManager.registerEvent(Type.PLUGIN_DISABLE, economyServerListener, Priority.Monitor, plugin);
+    }
+
+    @Override
+    public boolean isEnabled() {
+        if(economy == null) {
+            return false;
+        } else {
+            return economy.isEnabled();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String format(double amount) {
+        return String.format("%d %s", iConomy.getBank().format(amount));
+    }
+
+    @Override
+    public double getBalance(String playerName) {
+        Account account = iConomy.getBank().getAccount(playerName);
+        if (account == null) {
+            iConomy.getBank().addAccount(playerName);
+            account = iConomy.getBank().getAccount(playerName);
+        }
+        return account.getBalance();
+    }
+
+    @Override
+    public boolean withdrawPlayer(String playerName, double amount) {
+        double balance = getBalance(playerName);
+        if(balance >= amount) {
+            Account account = iConomy.getBank().getAccount(playerName);
+            if(account == null) {
+                return false;
+            }
+            account.subtract(amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean depositPlayer(String playerName, double amount) {
+        Account account = iConomy.getBank().getAccount(playerName);
+        if(account == null) {
+            iConomy.getBank().addAccount(playerName);
+            account = iConomy.getBank().getAccount(playerName);
+        }
+        account.add(amount);
+        return false;
+    }
+
+    @Override
+    public boolean withdrawShop(Shop shop, double amount) {
+        // Currently not supported
+        return false;
+    }
+
+    @Override
+    public boolean depositShop(Shop shop, double amount) {
+        // Currently not supported
+        return false;
+    }
+    
+    private class EconomyServerListener extends ServerListener {
+        Economy_iConomy4 economy = null;
+        
+        public EconomyServerListener(Economy_iConomy4 economy) {
+            this.economy = economy;
+        }
+        
+        public void onPluginEnable(PluginEnableEvent event) {
+            if (economy.economy == null) {
+                Plugin iConomy = plugin.getServer().getPluginManager().getPlugin("iConomy");
+
+                if (iConomy != null) {
+                    if (iConomy.isEnabled() && iConomy instanceof com.nijiko.coelho.iConomy.iConomy) {
+                        economy.economy = (iConomy) iConomy;
+                        log.info(String.format("[%s] %s hooked.", plugin.getDescription().getName(), economy.name));
+                    }
+                }
+            }
+        }
+        
+        public void onPluginDisable(PluginDisableEvent event) {
+            if (economy.economy != null) {
+                if (event.getPlugin().getDescription().getName().equals("iConomy")) {
+                    economy.economy = null;
+                    log.info(String.format("[%s] %s un-hooked.", plugin.getDescription().getName(), economy.name));
+                }
+            }
+        }
+    }
+}
