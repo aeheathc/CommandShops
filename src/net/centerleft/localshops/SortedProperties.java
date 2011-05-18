15767f296201ec47ea2f6ad0c78f223617deef03
Morgan <mrh9724@lanaddict.com>
2011-05-18 19:43:29 -0400

    Formatting

--- src/net/centerleft/localshops/SortedProperties.java
+++ src/net/centerleft/localshops/SortedProperties.java
@@ -10,13 +10,12 @@ public class SortedProperties extends Properties {
 
     @SuppressWarnings({ "rawtypes", "unchecked" })
     public Enumeration keys() {
-       Enumeration keysEnum = super.keys();
-       Vector<String> keyList = new Vector<String>();
-       while(keysEnum.hasMoreElements()){
-         keyList.add((String)keysEnum.nextElement());
-       }
-       Collections.sort(keyList);
-       return keyList.elements();
+        Enumeration keysEnum = super.keys();
+        Vector<String> keyList = new Vector<String>();
+        while (keysEnum.hasMoreElements()) {
+            keyList.add((String) keysEnum.nextElement());
+        }
+        Collections.sort(keyList);
+        return keyList.elements();
     }
-    
-  }
+}
